26be9b6476609319277e3a92d4129acc7c711ec8
ferraricharles <charles.cjf@gmail.com>
2017-09-28 15:27:04 +0200

    Suggestion how to implement Person.Java

--- Class 2/Person.java
+++ Class 2/Person.java
@@ -1,21 +1,65 @@
-// Again here, Class name with capital letter
-public class Person{
-  // private property name
-  private String name;
 
-  public void setName(String name){
-    this.name = name;
-  }
+public class Person {
+	private String name;
+	private int age;
+	private char gender;
+	private boolean pregnant;
 
-  public String getName(){
-    return name;
-  }
-  /*
-    An ampty constructor, you don't need to implement it usually,
-    but let's say you want to start something with the object, here it comes
-  */
-  public Person(){
+  // My constructor with all arguments
+	public Person(String name, int age, char gender, boolean pregnant) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.pregnant = pregnant;
+	}
 
-  }
+  // My equals method
+	public boolean equals(Person other) {
+		if (age != other.age)
+			return false;
+		if (gender != other.gender)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (pregnant != other.pregnant)
+			return false;
+		return true;
+	}
+
+  // Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public boolean isPregnant() {
+		return pregnant;
+	}
+
+	public void setPregnant(boolean pregnant) {
+		this.pregnant = pregnant;
+	}
 
 }
