dbad1dc07cfc3d9a83bfb5cfe1ed7b78bb40bd73
ferraricharles <charles.cjf@gmail.com>
2017-10-15 23:11:31 +0200

    adding fifth class

---
+++ Class 5/Test.java
@@ -0,0 +1,13 @@
+import models.MyMatrix;
+
+public class Test{
+
+  public static void main(String args[]){
+
+    int[][] john;
+    john = new int[4][4];
+
+    MyMatrix m = new MyMatrix(4);
+    m.addToMatrix(4,1,10);
+  }
+}
